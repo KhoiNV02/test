aa797c347d4fc8904538dbc407e72dfda584f845
KhoiNV02 <khoikakaka123@gmail.com>
2021-10-03 22:15:22 +0700

    thay doi xong cai nay la ok

--- Ctest.cs
+++ Ctest.cs
@@ -15,7 +15,7 @@ namespace alpha_test
     
         public void kaka()
         {
-            Console.WriteLine(" phai la haha nua ne");
+            Console.WriteLine(" la haha nua ne");
         }       
     }
  public  class ok 
