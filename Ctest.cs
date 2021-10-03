31b806b66b06b8d92ba35124a58423b1cd9db692
KhoiNV02 <khoikakaka123@gmail.com>
2021-10-03 21:50:21 +0700

    t moi them cai moi nè

---
+++ Ctest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace alpha_test
+{
+  /*  public interface so
+    {
+        void read();
+        void Write();
+    }*/
+  public  class Ctest
+    {
+        public int a, b;
+    
+        public void kaka()
+        {
+            Console.WriteLine("haha");
+        }       
+    }
+ public  class ok 
+    {
+        public static void read1 (dynamic ob)
+        {
+            ob.kaka();
+        }
+       
+    }
+}
